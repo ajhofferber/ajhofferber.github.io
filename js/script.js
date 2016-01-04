bfdb764700d5cf0abe47327a973f69eb9f894a03
ruby -v ruby -v <ajhofferber@gmail.com>
2016-01-04 12:48:27 -0500

    updates site

--- js/script.js
+++ js/script.js
@@ -1,2 +1,39 @@
 
-console.log("hey")
+$(document).ready( function(){
+
+  
+
+  $('#overlay').click( function(){
+    $("#overlay").css("display", "none");
+  });
+
+  $('span:hidden').fadeIn( "slow")
+
+
+  $(function(){
+    var $jittery = $('.jittery'),
+        aText    = $jittery.text().split(''),
+        letters = '';
+
+    for(var i = 0; i < aText.length; i++){
+      letters += '<span>'+aText[i]+'</span>';
+
+    }
+
+    $jittery.empty().append(letters);
+
+    $.each($('span', $jittery), function(i){
+      $(this).css('animation-delay', '-'+i+'70ms');
+    });
+  });
+
+
+
+
+  $('#back').click( function(){
+    $("#overlay").css("display", "block");
+  })
+
+
+
+})

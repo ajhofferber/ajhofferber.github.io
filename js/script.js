72176a84b998b4a7a64d7e9db46b23bbdc8af115
ruby -v ruby -v <ajhofferber@gmail.com>
2016-02-18 11:51:27 -0500

    adds new text on main overlay

--- js/script.js
+++ js/script.js
@@ -1,38 +1,47 @@
 
+function doIt() {
+  var $jittery = $('.jittery'),
+      aText    = $jittery.text().split(''),
+      letters = '';
+  for(var i = 0; i < aText.length; i++){
+    letters += '<span class="jittery">'+aText[i]+'</span>';
+  }
+  $jittery.empty().append(letters);
+
+  // $.each($('span', $jittery), function(i){
+  //   $(this).css('animation-delay', '-'+i+'70ms');
+  // });
+};
+
+$('#overlay').click( function(){
+  $("#overlay").addClass("closed");
+  $("#main").fadeIn(1000)
+});
+
+
+$('#back').click( function(){
+  $("#overlay").addClass("open");
+})
+
+function fillLetters(){
+  $('path').css("fill", "#333399");
+  $('path').css("stroke", "#000000");
+  $('path').css("stroke-width", "1.5");
+}
+
 $(document).ready( function(){
+    doIt();
+    setTimeout(fillLetters, 2000);
 
-  
-
-  $('#overlay').click( function(){
-    $("#overlay").css("display", "none");
-  });
 
   $('span:hidden').fadeIn( "slow")
 
-
-  $(function(){
-    var $jittery = $('.jittery'),
-        aText    = $jittery.text().split(''),
-        letters = '';
-
-    for(var i = 0; i < aText.length; i++){
-      letters += '<span>'+aText[i]+'</span>';
-
-    }
-
-    $jittery.empty().append(letters);
-
-    $.each($('span', $jittery), function(i){
-      $(this).css('animation-delay', '-'+i+'70ms');
-    });
+  $('#overlay').click( function(){
+    $("#overlay").addClass("closed");
+    $("#main").fadeIn(1000)
   });
 
 
-
-
-  $('#back').click( function(){
-    $("#overlay").css("display", "block");
-  })
 
 
 
